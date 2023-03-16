57afdbac35806c64e32e8f989f0a427927a2fd2f
Hossein Araghi <hoseinaraghi84@gmail.com>
2023-03-16 23:59:00 +0330

    Some changes on Button and Input components

--- components/tk/input.vue
+++ components/tk/input.vue
@@ -3,7 +3,8 @@ const props = defineProps<{
     placeholder?: string | undefined,
     type?: "text" | "email" | "password" | undefined,
     size?: "large" | "medium",
-    required?: boolean
+    required?: boolean,
+    name?: string
 }>()
 
 const spanEl: Ref<null> | Ref<HTMLSpanElement> = ref(null)
@@ -33,7 +34,7 @@ const changeVisibility = () =>
     <div class="tk_input relative flex items-center justify-end">
         <input class="px-4 py-3 pr-[45px] rounded-md focus:!outline-primary border-2 w-full" 
             :type="type ? type : 'text'" :placeholder="placeholder" ref="inputEl" :required="required"
-            :class="{ 'px-3 py-2': size === 'medium' }">
+            :class="{ 'px-3 py-2': size === 'medium' }" :name="name">
 
             <span class="material-symbols-outlined absolute right-3 p-2 !text-[20px] hover:bg-gray-50 rounded-full cursor-pointer select-none"
                  v-if="type === 'password'" data-status="hide" @click="changeVisibility" ref="spanEl">
